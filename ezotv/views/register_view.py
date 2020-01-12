bce0caf09c5b6ee7dd3c071ded8e1bea1d080355
marcsello <punkosdmarcell@rocketmail.com>
2020-01-12 15:42:12 +0100

    Added form prefill

--- ezotv/views/register_view.py
+++ ezotv/views/register_view.py
@@ -17,13 +17,15 @@ class RegisterView(FlaskView):
 
     def get(self):
 
-        return render_template('register.html', config={"recaptcha_sitekey": current_app.config["RECAPTCHA_SITEKEY"]})
+        return render_template('register.html', config={"recaptcha_sitekey": current_app.config["RECAPTCHA_SITEKEY"]}, prefill={})
 
     def post(self):
 
+        form_raw_data = dict(request.form)
+
         try:
-            form_contents = self.register_form_schema.load(dict(request.form))
+            form_data = self.register_form_schema.load(form_raw_data)
         except ValidationError as e:
-            return render_template('register.html', error=str(e), config={"recaptcha_sitekey": current_app.config["RECAPTCHA_SITEKEY"]})
+            return render_template('register.html', error=str(e.normalized_messages()), config={"recaptcha_sitekey": current_app.config["RECAPTCHA_SITEKEY"]}, prefill=form_raw_data)
 
         return render_template('register_success.html')
