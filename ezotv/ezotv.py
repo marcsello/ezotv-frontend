5b295b0da2ff01a816b28d82d068d2931a40593b
Marcsello <marcsello@users.noreply.github.com>
2020-01-18 21:21:57 +0100

    Fixed not failing on missing envvars

--- ezotv/ezotv.py
+++ ezotv/ezotv.py
@@ -23,15 +23,15 @@ from api_views import UserView
 app = Flask(__name__)
 
 # configure flask app
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['EZOTV_DATABASE_URI']
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('EZOTV_DATABASE_URI',"sqlite://") # Default to memory db
 app.config['LOCAL_API_KEY'] = os.environ['EZOTV_LOCAL_API_KEY']
 app.config['LUNA_API_KEY'] = os.environ['EZOTV_LUNA_API_KEY']
 app.config['SERVER_NAME'] = os.environ['EZOTV_SERVER_NAME']  # ezotv.marcsello.com
 
 # important stuff
-app.secret_key = os.environ.get("FLASK_SECRET_KEY", os.urandom(12))
-app.config["DISCORD_OAUTH_CLIENT_ID"] = os.environ.get("EZOTV_DISCORD_OAUTH_CLIENT_ID")
-app.config["DISCORD_OAUTH_CLIENT_SECRET"] = os.environ.get("EZOTV_DISCORD_OAUTH_CLIENT_SECRET")
+app.secret_key = os.environ.get('FLASK_SECRET_KEY', os.urandom(12))
+app.config['DISCORD_OAUTH_CLIENT_ID'] = os.environ['EZOTV_DISCORD_OAUTH_CLIENT_ID']
+app.config['DISCORD_OAUTH_CLIENT_SECRET'] = os.environ['EZOTV_DISCORD_OAUTH_CLIENT_SECRET']
 
 
 # initialize stuff
