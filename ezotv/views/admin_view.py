988b19479b9fcaa1169c782400ae88926fddfb83
marcsello <punkosdmarcell@rocketmail.com>
2020-01-21 04:12:21 +0100

    finished admin view post handler

--- ezotv/views/admin_view.py
+++ ezotv/views/admin_view.py
@@ -6,7 +6,7 @@ from flask_login import login_required, current_user, logout_user
 
 from urllib.parse import urljoin, quote
 
-from model import db, User, NameChange
+from model import db, User, NameChange, NameStatus
 from utils import DiscordBot
 
 
@@ -46,3 +46,37 @@ class AdminView(FlaskView):
 
         return render_template("admin.html", name_changes=name_changes, extra_info=extra_info)
 
+    def post(self):  # TODO: Marshmallow? ?? ?
+
+        approved = 'verdict_accept' in request.form
+        try:
+            namechange_id = int(request.form['id'])
+        except ValueError:
+            flash("Hiba a bevitt adatokban!", "danger")  # ezt valaki tuti humorosnak véli majd, tekintve, hogy a bevitt adatok az két gomb
+            return redirect(url_for("AdminView:index"))
+
+        namechange = NameChange.query.get(namechange_id)
+        user = namechange.user
+
+        if not namechange:
+            flash("Ismeretlen kérvény!", "danger")
+            return redirect(url_for("AdminView:index"))
+
+        if approved:
+            user.name_status = NameStatus.APPROVED
+        else:
+            user.name_status = NameStatus.UNAPPROVED
+
+        user.name_status.in_sync = False
+
+        db.session.add(user)
+        db.session.delete(namechange)
+
+        db.session.commit()  # Itt nem sérthetünk meg constraint
+
+        if approved:
+            flash("{} név elfogadva!".format(user.minecraft_name), "success")
+        else:
+            flash("{} név elutasítva!".format(user.minecraft_name), "warning")
+
+        return redirect(url_for("AdminView:index"))
